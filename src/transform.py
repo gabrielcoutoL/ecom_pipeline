cf91d6223ab07f53fa951a47db4c75be191135af
gabrielcoutoL <79060224+gabrielcoutoL@users.noreply.github.com>
2026-07-17 15:09:53 -0300

    transform

--- src/transform.py
+++ src/transform.py
@@ -24,18 +24,32 @@ def anula_valores_negativos(df: pd.DataFrame, col: str) -> pd.DataFrame:
 
     negativos = d[col] < 0
 
-    d.loc[negativos, col] = pd.NaT
+    d.loc[negativos, col] = pd.NA
 
     logger.info("%s coluna anulando %d valores.", col, sum(negativos))
 
     return d
 
 
+def valida_fk_orfas(df_direita: pd.DataFrame, df_esquerda: pd.DataFrame, col: str):
+    d = df_direita.copy()
+
+    ids_validos = set(df_esquerda[col])
+
+    mask = d[col].isin(ids_validos)
+
+    logger.info("%d valores setados para desconhecido", (~mask).sum())
+
+    d.loc[~mask, col] = "Desconhecido"
+
+    return d
+
+
 # TRANSFORMAÇÕES DOS DATAFRAMES
-def transform_customers(df: pd.DataFrame) -> pd.DataFrame:
+def transform_customers(df_customers: pd.DataFrame) -> pd.DataFrame:
 
     return (
-        df.dropna(subset="customer_id")
+        df_customers.dropna(subset="customer_id")
         .drop_duplicates(subset="customer_id")
         .assign(
             customer_name=lambda d: d["customer_name"].str.strip().str.title(),
@@ -59,10 +73,10 @@ def transform_customers(df: pd.DataFrame) -> pd.DataFrame:
     )
 
 
-def transform_order_items(df: pd.DataFrame) -> pd.DataFrame:
+def transform_order_items(df_order_items: pd.DataFrame) -> pd.DataFrame:
 
     return (
-        df.dropna(subset="order_item_id")
+        df_order_items.dropna(subset="order_item_id")
         .drop_duplicates(subset="order_item_id")
         .astype(
             {
@@ -79,5 +93,27 @@ def transform_order_items(df: pd.DataFrame) -> pd.DataFrame:
         .pipe(anula_valores_negativos, col="price")
         .pipe(anula_valores_negativos, col="freight_value")
         .replace({"quantity": {0: pd.NA}})
+        .reset_index(drop=True)
+    )
+
+
+def transform_orders(
+    df_orders: pd.DataFrame, df_customers: pd.DataFrame
+) -> pd.DataFrame:
+
+    return (
+        df_orders.dropna(subset="order_id")
+        .drop_duplicates(subset="order_id")
+        .pipe(valida_fk_orfas, df_esquerda=df_customers, col="customer_id")
+        .assign(order_status=lambda d: d["order_status"].str.strip().str.title())
+        .astype(
+            {
+                "order_id": str,
+                "customer_id": str,
+                "order_status": str,
+                "purchase_ts": "datetime64[ns]",
+                "data_extracao": "datetime64[ns]",
+            }
+        )
         .reset_index(drop=True)
     )
